2dd23866ed73073abe5b759961e38611528cfe36
christianhunger <christian.hunger@exxcellent.de>
2019-01-07 10:35:45 +0100

    docs(IdGeneratorUtil): add jsdoc-comment for all exported functions

--- src/util/IdGeneratorUtil.js
+++ src/util/IdGeneratorUtil.js
@@ -1,10 +1,35 @@
-const randomIntInRange = (min, max) => {
+/**
+ * Util to generate ids.
+ * @module IdGeneratorUtil
+ */
+
+/**
+ * Geenrates a random integer id in the range [min, max].
+ * Throws an error if max <= min!
+ *
+ * @param {number} min - The lower bound.
+ * @param {number} max - The upper bound.
+ * @returns {number}
+ */
+export const randomIntInRange = (min, max) => {
+  if (max <= min) {
+    throw "Invalid arguments: max <= min.";
+  }
+
   min = Math.ceil(min);
   max = Math.floor(max);
   const result = Math.floor(Math.random() * (max - min)) + min;
   return result;
 };
 
+/**
+ * Generates two distinct integer ids in the range [min, max].
+ * Throws an error if max <= min!
+ *
+ * @param {number} min - The lower bound.
+ * @param {number} max - The upper bound.
+ * @returns {{firstId: *, secondId: *}} - A tuple containing the ids.
+ */
 export const twoDifferentRandomIdsInRange = (min, max) => {
   if (max <= min) {
     throw "Invalid arguments: max <= min.";
