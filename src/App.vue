821a3157f182772ab210999747989792fc2805c2
David Müller <david.mueller@exxcellent.de>
2019-01-21 20:55:51 +0100

    refactor(*): prepare starter branch

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,30 @@
 <template>
-  <div id="app">
-    <img src="logo.png" width="64"/>
-    <Comparator :id1="currentRound.sample1.contenderId"
-                :id2="currentRound.sample2.contenderId"
-                :imageUrl1="currentRound.sample1.sampleUrl"
-                :imageUrl2="currentRound.sample2.sampleUrl"
-                @winner="handleWinner"/>
-    <!-- <pre>{{ currentContenders }}</pre> -->
-  </div>
+    <div id="app">
+        <img src="logo.png" width="64"/>
+        <div class="columns">
+            <div class="column">
+                A
+            </div>
+
+            <div class="column">
+                B
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import { kittens } from "@/util/KittenTestdata.js";
-import Comparator from "@/components/Comparator";
-import { currentContenderIdRange, updateRating } from "@/util/contenders";
-import { twoDifferentRandomIdsInRange } from "./util/IdGeneratorUtil";
 
 export default {
   name: "App",
   components: {
-    Comparator
+    // Comparator
   },
   data() {
     return {
-      currentContenders: kittens,
-      contenderId1: null,
-      contenderId2: null
+      currentContenders: kittens
     };
-  },
-  mounted() {
-    // @See https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
-    this.startNextRound();
-  },
-  computed: {
-    currentRound() {
-      return {
-        sample1: {
-          contenderId: this.contenderId1,
-          sampleUrl: this.contenderId1
-            ? this.currentContenders[this.contenderId1].imageUrl
-            : ""
-        },
-        sample2: {
-          contenderId: this.contenderId2,
-          sampleUrl: this.contenderId2
-            ? this.currentContenders[this.contenderId2].imageUrl
-            : ""
-        }
-      };
-    }
-  },
-  methods: {
-    handleWinner(winnerId) {
-      const result = {
-        contenderId1: this.contenderId1,
-        contenderId2: this.contenderId2,
-        winnerId
-      };
-
-      this.currentContenders = updateRating(this.currentContenders, result);
-
-      this.startNextRound();
-    },
-    startNextRound() {
-      const { min, max } = currentContenderIdRange(this.currentContenders);
-      const { firstId, secondId } = twoDifferentRandomIdsInRange(min, max);
-      this.contenderId1 = firstId;
-      this.contenderId2 = secondId;
-    }
   }
 };
 </script>
