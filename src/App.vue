73f03857388aa1da2c27b237ecfc583c15915ada
David Müller <david.mueller@exxcellent.de>
2019-01-21 18:19:21 +0100

    refactor(*): remove ranking strip and play container component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,73 @@
 <template>
   <div id="app">
     <img src="logo.png" width="64"/>
-    <play-component/>
+    <Comparator :id1="currentRound.sample1.contenderId"
+                :id2="currentRound.sample2.contenderId"
+                :imageUrl1="currentRound.sample1.sampleUrl"
+                :imageUrl2="currentRound.sample2.sampleUrl"
+                @winner="handleWinner"/>
+    <!-- <pre>{{ currentContenders }}</pre> -->
   </div>
 </template>
 
 <script>
-import PlayComponent from "@/views/Play";
+import { kittens } from "@/util/KittenTestdata.js";
+import Comparator from "@/components/Comparator";
+import { currentContenderIdRange, updateRating } from "@/util/contenders";
+import { twoDifferentRandomIdsInRange } from "./util/IdGeneratorUtil";
 
 export default {
   name: "App",
   components: {
-    PlayComponent
+    Comparator
+  },
+  data() {
+    return {
+      currentContenders: kittens,
+      contenderId1: null,
+      contenderId2: null
+    };
+  },
+  mounted() {
+    // @See https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
+    this.startNextRound();
+  },
+  computed: {
+    currentRound() {
+      return {
+        sample1: {
+          contenderId: this.contenderId1,
+          sampleUrl: this.contenderId1
+            ? this.currentContenders[this.contenderId1].imageUrl
+            : ""
+        },
+        sample2: {
+          contenderId: this.contenderId2,
+          sampleUrl: this.contenderId2
+            ? this.currentContenders[this.contenderId2].imageUrl
+            : ""
+        }
+      };
+    }
+  },
+  methods: {
+    handleWinner(winnerId) {
+      const result = {
+        contenderId1: this.contenderId1,
+        contenderId2: this.contenderId2,
+        winnerId
+      };
+
+      this.currentContenders = updateRating(this.currentContenders, result);
+
+      this.startNextRound();
+    },
+    startNextRound() {
+      const { min, max } = currentContenderIdRange(this.currentContenders);
+      const { firstId, secondId } = twoDifferentRandomIdsInRange(min, max);
+      this.contenderId1 = firstId;
+      this.contenderId2 = secondId;
+    }
   }
 };
 </script>
--- src/util/contenders.js
+++ src/util/contenders.js
@@ -27,22 +27,6 @@ export const updateRating = (
 
 /**
  * @param contenders
- * @returns {any[]}
- */
-export const sortContendersByTheirRating = contenders => {
-  // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries.
-  return Object.entries(contenders)
-    .map(entry => {
-      return {
-        id: entry[0],
-        ...entry[1]
-      };
-    })
-    .sort((contender1, contender2) => contender2.rating - contender1.rating);
-};
-
-/**
- * @param contenders
  * @returns {{min: number, max: number}}
  */
 export const currentContenderIdRange = contenders => {
