25cbe4fb986693cb286e5dc61a420da3aade0eae
David Müller <david@dmwe.de>
2019-01-22 21:24:54 +0100

    test(e2e): make cypress example test pass

--- src/store/store.js
+++ src/store/store.js
@@ -12,7 +12,7 @@ export default new Vuex.Store({
   state: {
     config: {
       server: {
-        enabled: true,
+        enabled: !window.Cypress,
         polling: {
           enabled: true,
           interval: 5000,
